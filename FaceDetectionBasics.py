70214c34bb7a2c62261a3112d81a44471a2d741f
updaun <updauney@daum.net>
2021-05-02 19:20:20 +0900

    face detection with mediapipe

--- FaceDetectionBasics.py
+++ FaceDetectionBasics.py
@@ -2,19 +2,34 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 pTime = 0
 
 mpFaceDetection = mp.solutions.face_detection
 mpDraw = mp.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection()
+faceDetection = mpFaceDetection.FaceDetection(0.75)
 
 while True:
     success, img = cap.read()
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # results = faceDetection.process(imgRGB)
-    # print(results)
+    results = faceDetection.process(imgRGB)
+    print(results)
+
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            # mpDraw.draw_detection(img, detection)
+            # print(detection)
+            # print(detection.score)
+            print(detection.location_data.relative_bounding_box)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 2)
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
